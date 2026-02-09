f62ae3d52b3dbe891c6c661a79bea1e1fcd16d14
carlosbetescalera-source <carlosbetescalera@gmail.com>
2026-02-09 11:33:28 +0100

    Initialize BettingRubricTrackerApp with state objects

--- BettingRubricTrackerApp.swift
+++ BettingRubricTrackerApp.swift
@@ -1,1 +1,17 @@
-Commit changes
+import SwiftUI
+
+@main
+struct BettingRubricTrackerApp: App {
+    @StateObject var appState = AppState()
+    @StateObject var rulesEngine = RulesEngine()
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HomeView()
+                    .environmentObject(appState)
+                    .environmentObject(rulesEngine)
+            }
+        }
+    }
+}
